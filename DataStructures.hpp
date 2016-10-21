3c53672e4919882a039204b889d7591106bbbcba
Hung Dang <hungptit@gmail.com>
2016-10-21 02:25:45 -0400

    Update README and do some minor cleanups.

--- DataStructures.hpp
+++ DataStructures.hpp
@@ -25,7 +25,7 @@ namespace graph {
         index_type SrcId;
         index_type DstId;
 
-        BasicEdgeData() {}
+        BasicEdgeData() noexcept {}
 
         BasicEdgeData(const index_type srcId, const index_type dstId) noexcept
             : SrcId(srcId), DstId(dstId) {}
@@ -111,15 +111,15 @@ namespace graph {
         using vertex_container = std::vector<index_type>;
         using edge_iterator = typename edge_container::const_iterator;
 
-        explicit SparseGraph() noexcept : Vertexes(), Edges(), IsDirected() {}
+        SparseGraph() noexcept : Vertexes(), Edges(), IsDirected()  {}
 
         template <typename T>
-        explicit SparseGraph(T &&data, const std::size_t N, const bool isDirected) noexcept
+        SparseGraph(T &&data, const std::size_t N, const bool isDirected) noexcept
             : Vertexes(), Edges(), IsDirected(isDirected) {
-            build(data, N);
+            build(std::forward<T>(data), N);
         }
 
-        explicit SparseGraph(vertex_container &&v, edge_container &&e, bool isDirected) noexcept
+        SparseGraph(vertex_container &&v, edge_container &&e, bool isDirected) noexcept
             : Vertexes(std::move(v)), Edges(std::move(e)), IsDirected(isDirected) {}
 
         template <typename Container>
@@ -173,10 +173,11 @@ namespace graph {
     }
 }
 
+// Customized hash function for different edge type.
 namespace std {
     template <typename index_type> struct hash<graph::BasicEdgeData<index_type>> {
         using result_type = std::size_t;
-        result_type operator()(graph::BasicEdgeData<index_type> const value) const {
+        result_type operator()(graph::BasicEdgeData<index_type> const &value) const {
             result_type const h1(std::hash<index_type>()(value.SrcId));
             result_type const h2(std::hash<index_type>()(value.DstId));
             return h1 ^ (h2 << 4);
@@ -187,7 +188,7 @@ namespace std {
     struct hash<graph::WeightedEdgeData<index_type, weight_type>> {
         using result_type = std::size_t;
         result_type
-        operator()(graph::WeightedEdgeData<index_type, weight_type> const value) const {
+        operator()(graph::WeightedEdgeData<index_type, weight_type> const &value) const {
             result_type const h1(std::hash<index_type>()(value.SrcId));
             result_type const h2(std::hash<index_type>()(value.DstId));
             result_type const h3(std::hash<weight_type>()(value.Weight));
--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # graph
-This is a simple graph library that is written in modern C++. **graph** uses [Column Sparse Row (CSR)](https://en.wikipedia.org/wiki/Sparse_matrix#Compressed_sparse_row_.28CSR.2C_CRS_or_Yale_format.29) format to store the graph information and it provides the following features:
 
-* Support fast serialization/deserialization using cereal and leveldb.
+This is a simple and high performance graph library written in modern C++ i.e C++11 and C++14. **graph** can be suitable for high performance applications that do not need to modify the graph and require very fast serialization/deserialization.
 
-* Display the graph information in JSON or XML formats.
+## Features ##
 
-* Implementation for some basic recursive and non-recursimve graph algorithms traversal algorithms such as DFS, BFS etc.
+* Use [Column Sparse Row (CSR)](https://en.wikipedia.org/wiki/Sparse_matrix#Compressed_sparse_row_.28CSR.2C_CRS_or_Yale_format.29) format to store the graph information.
+
+* Support very fast serialization/deserialization using [cereal](http://uscilab.github.io/cereal/).
+
+* Provide several basic recursive and non-recursive graph traversal algorithms such as DFS and BFS.
+
+* Output the graph information in DOT, JSON, and XML formats.
+
